bb2339d1d69b88c78698fe13bc05a24bcfeaf657
Edeltraud Genzel <115988769+edeltraud1234@users.noreply.github.com>
2022-10-17 12:49:22 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title ('my parents new healthy dinner')
 streamlit.header ('Breakfast')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, spinach & rocket smoothies')
-streamlit.text ('hard boiled free range Egg')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗Kale, spinach & rocket smoothies')
+streamlit.text ('🐔hard boiled free range Egg')
+streamlit.text ('🥑 avocado toast')
