ed06036039e25e24dfc15564320a0ac9fa7eb748
Edeltraud Genzel <115988769+edeltraud1234@users.noreply.github.com>
2022-10-17 12:55:49 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,8 +1,3 @@
-import streamlit
-streamlit.title ('my parents new healthy dinner')
-streamlit.header ('Breakfast')
-streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text ('🥗Kale, spinach & rocket smoothies')
-streamlit.text ('🐔hard boiled free range Egg')
-streamlit.text ('🥑 avocado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list =pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
