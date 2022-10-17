493c1bf26cd49db4b637d9af66e2c4298ac4491e
Edeltraud Genzel <115988769+edeltraud1234@users.noreply.github.com>
2022-10-17 13:27:16 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,1 @@
 import streamlit
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
