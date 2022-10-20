9ce6f08612a7d7085628dcd60817d618bf40235e
Edeltraud Genzel <115988769+edeltraud1234@users.noreply.github.com>
2022-10-20 17:01:36 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -30,7 +30,9 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
+database = "pc_rivery_db" 
+schema = "public"
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("contain")
 streamlit.text(my_data_row)
